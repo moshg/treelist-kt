01f95e1f747d2daf13bd193e0d2204ebc2511a7f
mosh <mosh.ohayo@gmail.com>
2019-02-14 01:17:51 +0900

    無駄な一時変数をなくした. varをvalに置き換えた.

--- test/NodeTest.kt
+++ test/NodeTest.kt
@@ -4,45 +4,45 @@ import org.junit.jupiter.api.Test
 internal class NodeTest {
     @Test
     fun get() {
-        val leaves = arrayOfNulls<Any?>(Node.B)
-        leaves[0] = 0
-        leaves[1] = 2
+        val leaves = arrayOfNulls<Any?>(Node.B).also {
+            it[0] = 0
+            it[1] = 2
+        }
         val node = Node<Int>(null, leaves)
         assertEquals(2, node.get(0, 1))
         assertEquals(leaves, node.getLeaves(0, 1))
 
-        val nodes = arrayOfNulls<Node<Int>?>(Node.B)
-        nodes[0] = Node<Int>(null, Array(Node.B) { it })
-        nodes[1] = node
-        val root = Node(nodes, null)
-        assertEquals(2, root.get(Node.WIDTH, Node.B + 1))
-        assertEquals(leaves, root.getLeaves(Node.WIDTH, Node.B + 1))
+        val node2 = Node(arrayOfNulls<Node<Int>?>(Node.B).also { nodes ->
+            nodes[0] = Node<Int>(null, Array(Node.B) { it })
+            nodes[1] = node
+        }, null)
+        assertEquals(2, node2.get(Node.WIDTH, Node.B + 1))
+        assertEquals(leaves, node2.getLeaves(Node.WIDTH, Node.B + 1))
     }
 
     @Test
     fun createSingle() {
         val node = Node.createSingle(Node.WIDTH, 2)
         assertEquals(null, node.leaves)
-        val child = node.nodes!![0]!!
+
         for (i in 1 until Node.B) {
             assertEquals(null, node.nodes!![i])
         }
+        val child = node.nodes!![0]!!
         assertEquals(null, child.nodes)
         assertEquals(2, child.leaves!![0])
     }
 
     @Test
     fun added() {
-        var node = Node<Int>(null, arrayOfNulls(Node.B))
-        node = node.added(0, 0, 1)
-        node = node.added(0, 1, 2)
+        val node = Node<Int>(null, arrayOfNulls(Node.B))
+            .added(0, 0, 1)
+            .added(0, 1, 2)
         assertEquals(1, node.get(0, 0))
         assertEquals(2, node.get(0, 1))
 
-        val nodes = arrayOfNulls<Node<Int>?>(Node.B)
-        nodes[0] = node
-        node = Node(nodes, null)
-        node = node.added(Node.WIDTH, Node.B, 100)
-        assertEquals(100, node.get(Node.WIDTH, Node.B))
+        val node2 = Node(arrayOfNulls<Node<Int>?>(Node.B).also { it[0] = node }, null)
+            .added(Node.WIDTH, Node.B, 100)
+        assertEquals(100, node2.get(Node.WIDTH, Node.B))
     }
 }
