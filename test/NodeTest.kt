43f7ff38b25a311e186824e78783b262f0cc21c5
mosh <mosh.ohayo@gmail.com>
2019-02-13 23:03:27 +0900

    get, getLeavesのテストを追加.

---
+++ test/NodeTest.kt
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class NodeTest {
+    @Test
+    fun get() {
+        val leaves = arrayOfNulls<Any?>(Node.B)
+        leaves[0] = 0
+        leaves[1] = 2
+        val node = Node<Int>(null, leaves)
+        assertEquals(2, node.get(0, 1))
+        assertEquals(leaves, node.getLeaves(0, 1))
+
+        val nodes = arrayOfNulls<Node<Int>?>(Node.B)
+        nodes[0] = Node<Int>(null, Array(Node.B) { it })
+        nodes[1] = node
+        val root = Node(nodes, null)
+        assertEquals(2, root.get(Node.WIDTH, Node.B + 1))
+        assertEquals(leaves, root.getLeaves(Node.WIDTH, Node.B + 1))
+    }
+}
