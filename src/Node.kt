ceb72f717bade4c6c09721f854b5be71a9a5cea6
mosh <mosh.ohayo@gmail.com>
2019-02-13 17:57:06 +0900

    addedで再帰を使わないように改善. それに伴いフィールドをvarに変更.

--- src/Node.kt
+++ src/Node.kt
@@ -1,4 +1,4 @@
-internal class Node<T>(val children: Array<Node<T>?>?, val leaves: Array<Any?>?) {
+internal class Node<T>(var children: Array<Node<T>?>?, var leaves: Array<Any?>?) {
     @Suppress("NOTHING_TO_INLINE")
     inline fun get(index: Int, level: Int): T = get(this, index, level)
 
@@ -35,24 +35,35 @@ internal class Node<T>(val children: Array<Node<T>?>?, val leaves: Array<Any?>?)
             }
         }
 
-        // OPTIMIZE
         @JvmStatic
         fun <T> added(node: Node<T>, index: Int, level: Int, e: T): Node<T> {
-            return if (level == 0) {
-                val newLeaves = node.leaves!!.copyOf()
-                newLeaves[index and MASK] = e
-                Node(null, newLeaves)
-            } else {
-                val arrIndex = (index ushr level) and MASK
+            var level = level
+            val newNode = Node<T>(null, null)
+            // 現在初期化しているノード
+            var currNode = newNode
+
+            while (level > 0) {
                 val newChildren = node.children!!.copyOf()
-                val child = node.children[arrIndex]
-                newChildren[arrIndex] = if (child === null) {
-                    createSingle(level - WIDTH, e)
+                val arrIndex = (index ushr level) and MASK
+                val child = newChildren[arrIndex]
+                if (child === null) {
+                    newChildren[arrIndex] = createSingle(level - WIDTH, e)
+                    currNode.children = newChildren
+                    break
                 } else {
-                    added(child, index, level - WIDTH, e)
+                    currNode.children = newChildren
+                    currNode = child
                 }
-                Node(newChildren, null)
+                level -= WIDTH
             }
+
+            if (level == 0) {
+                val newLeaves = arrayOfNulls<Any?>(WIDTH)
+                newLeaves[index and MASK] = e
+                currNode.leaves = newLeaves
+            }
+
+            return newNode
         }
     }
 }
