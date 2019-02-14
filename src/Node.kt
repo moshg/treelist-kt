99b00f7decb0d441cfbd7893785298d297d969f4
mosh <mosh.ohayo@gmail.com>
2019-02-15 04:36:36 +0900

    addedLeavesとcreateSingleLeavesを追加.

--- src/Node.kt
+++ src/Node.kt
@@ -10,6 +10,9 @@ internal class Node<T>(var nodes: Array<Node<T>?>?, var leaves: Array<Any?>?) {
     inline fun added(level: Int, index: Int, e: T): Node<T> = added(this, level, index, e)
 
     @Suppress("NOTHING_TO_INLINE")
+    inline fun addedLeaves(level: Int, index: Int, leaves: Array<Any?>) = addedLeaves(this, level, index, leaves)
+
+    @Suppress("NOTHING_TO_INLINE")
     inline fun set(level: Int, index: Int, e: T): Node<T> = set(this, level, index, e)
 
     override fun toString(): String = buildString(B * 3) {
@@ -48,16 +51,18 @@ internal class Node<T>(var nodes: Array<Node<T>?>?, var leaves: Array<Any?>?) {
         inline fun getIndex(level: Int, i: Int): Int = (i ushr level) and MASK
 
         @JvmStatic
-        fun <T> createSingle(level: Int, e: T): Node<T> {
+        fun <T> createSingle(level: Int, e: T): Node<T> =
+            createSingleLeaves(level, arrayOfNulls<Any?>(Node.B).also { it[0] = e })
+
+        @JvmStatic
+        fun <T> createSingleLeaves(level: Int, leaves: Array<Any?>): Node<T> {
             var level = level
-            val elements = arrayOfNulls<Any?>(Node.B)
-            elements[0] = e
-            var node = Node<T>(null, elements)
+            var node = Node<T>(null, leaves)
             while (level > 0) {
                 val nodes = arrayOfNulls<Node<T>>(Node.B)
                 nodes[0] = node
                 node = Node(nodes, null)
-                level -= 5
+                level -= WIDTH
             }
             return node
         }
@@ -114,6 +119,42 @@ internal class Node<T>(var nodes: Array<Node<T>?>?, var leaves: Array<Any?>?) {
                 val newLeaves = currNode.leaves!!.copyOf()
                 newLeaves[index and MASK] = e
                 currNewNode.leaves = newLeaves
+            }
+
+            return retNode
+        }
+
+        @JvmStatic
+        fun <T> addedLeaves(node: Node<T>, level: Int, index: Int, leaves: Array<Any?>): Node<T> {
+            var level = level
+            val retNode = Node<T>(null, null)
+            // 現在初期化しているノード
+            var currNewNode = retNode
+            // 現在コピーしているノード
+            var currNode = node
+
+            while (level > 0) {
+                val currNewNodes = currNode.nodes!!.copyOf()
+                val arrIndex = (index ushr level) and MASK
+                val child = currNewNodes[arrIndex]
+                if (child === null) {
+                    currNewNodes[arrIndex] = createSingleLeaves(level - WIDTH, leaves)
+                    currNewNode.nodes = currNewNodes
+                    break
+                } else {
+                    // nextNewNodeをcurrNewNodeのChildrenに追加し, currNewNodeをnextNewNodeに差し替える.
+                    val nextNewNode = Node<T>(null, null)
+                    currNewNodes[arrIndex] = nextNewNode
+                    currNewNode.nodes = currNewNodes
+                    currNewNode = nextNewNode
+
+                    currNode = child
+                }
+                level -= WIDTH
+            }
+
+            if (level == 0) {
+                currNewNode.leaves = leaves
             }
 
             return retNode
