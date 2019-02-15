244b5efb72d3b6fc7056da4e3ac872c1c9c2cab9
mosh <mosh.ohayo@gmail.com>
2019-02-15 16:57:38 +0900

    buildのテストを追加.

---
+++ test/TreeListBuilderTest.kt
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TreeListBuilderTest {
+    @Test
+    fun build() {
+        val b = TreeListBuilder<Int>()
+
+        assert(b.build().isEmpty())
+
+        b.add(2)
+        b.add(3)
+        val l = b.build()
+        assertEquals(2, l[0])
+        assertEquals(3, l[1])
+
+        for (i in 0 until B * B + 1) {
+            b.add(i + 5)
+        }
+        val l2 = b.build()
+        assertEquals(2, l[0])
+        assertEquals(3, l[1])
+        for (i in 0 until B * B + 1) {
+            assertEquals(i + 5, l2[i])
+        }
+    }
+}
