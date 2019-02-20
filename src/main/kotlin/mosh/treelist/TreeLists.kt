4e7ca67115fb1bfb5c390f5e96b63922a7ac1fd7
mosh <mosh.ohayo@gmail.com>
2019-02-21 04:56:11 +0900

    emptyTreeListを追加.

--- src/main/kotlin/mosh/treelist/TreeLists.kt
+++ src/main/kotlin/mosh/treelist/TreeLists.kt
@@ -4,6 +4,12 @@ internal const val B: Int = 32
 internal const val WIDTH: Int = 5
 internal const val MASK: Int = (1 shl WIDTH) - 1
 
+private val EMPTY: TreeList<*> = TreeList<Any?>()
+
+@Suppress("UNCHECKED_CAST")
+fun <T> emptyTreeList(): TreeList<T> =
+    EMPTY as TreeList<T>
+
 fun <T> treeListOf(vararg elements: T): TreeList<T> = buildTreeList {
     for (e in elements) {
         add(e)
